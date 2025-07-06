21b59279ced96e1b61bd2aba31c95883e0c9c139
woojae <swjdnwo@gmail.com>
2025-07-06 20:47:45 +0900

    path test commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import reportWebVitals from './reportWebVitals';
 import './styles/main.scss'
+
+import Main from './layout/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    테스트 해볼거야
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pf" element={<Main/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/layout/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../assets/styles/main.scss';
+//import Header from '../layout/Header';
+//import Footer from '../layout/Footer';
+//import MenuDepth1TabList from '../component/MenuTabListDepth1';
+//import MenuDepth2TabList from '../component/MenuTabListDepth2';
+//import MenuDepth3TabList from '../component/MenuTabListDepth3';
+
+function Main(props){
+  //const [selectedDepth1, setSelectedDepth1] = useState('회계관리');
+  //const [selectedDepth2, setSelectedDepth2] = useState('재무회계');
+  
+
+  return (
+    <div className="main">
+      라훌라훌
+    </div>  
+  );
+}
+
+export default Main;
