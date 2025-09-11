d02fd1fd2c62a14307f43091533d29407490d8bb
woojae <swjdnwo@gmail.com>
2025-09-11 23:46:40 +0900

    0911 nano commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+//import react from "react";
+import '../assets/scss/main.scss';
+
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h2 className="myname">SHIN WOO JAE</h2>
+      </main>
+      <section className='listTest'>
+        <ul>
+          <li>
+            123
+          </li>
+          <li>
+            124
+          </li>
+          <li>
+            125
+          </li>
+          <li>
+            126
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+}
