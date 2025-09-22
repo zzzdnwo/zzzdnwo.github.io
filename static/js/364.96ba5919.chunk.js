858f7f4083a9b009005a7341b7b930720507d465
zzzdnwo <zzzdnwo@users.noreply.github.com>
2025-09-22 14:53:24 +0000

    deploy: 48eba9c04f76941cdb1f81f616d7603e60f4c509

---
+++ static/js/364.96ba5919.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkswj_portfolio=self.webpackChunkswj_portfolio||[]).push([[364],{364:(e,s,t)=>{t.r(s),t.d(s,{default:()=>l});t(43);var r=t(579);function l(){return(0,r.jsx)("div",{className:"project-detail",children:(0,r.jsx)("p",{children:"test"})})}}}]);
+//# sourceMappingURL=364.96ba5919.chunk.js.map
